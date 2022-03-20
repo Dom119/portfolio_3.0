7a71aa48d566ee93ed51a0ad0c41aa5b28d6a017
dom119 <quydung119@gmail.com>
2022-03-21 03:41:50 +1100

    Updates

---
+++ precache-manifest.eb7308bb4b0de48b354762c89a180d58.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c32c6609560392029fb03e209e85d6",
+    "url": "/portfolio_3.0/index.html"
+  },
+  {
+    "revision": "2dd052e20cf2de640e04",
+    "url": "/portfolio_3.0/static/css/main.98a3d110.chunk.css"
+  },
+  {
+    "revision": "b04b787a8f1fc429db7d",
+    "url": "/portfolio_3.0/static/js/2.56a5d674.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio_3.0/static/js/2.56a5d674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd052e20cf2de640e04",
+    "url": "/portfolio_3.0/static/js/main.24ce878d.chunk.js"
+  },
+  {
+    "revision": "60e0994ea92f0c2d54d3",
+    "url": "/portfolio_3.0/static/js/runtime-main.c875be67.js"
+  },
+  {
+    "revision": "e777e2ba809a6e34d335aafb4ef4674c",
+    "url": "/portfolio_3.0/static/media/Breaking-Ball.e777e2ba.png"
+  },
+  {
+    "revision": "07849fb65990656bf88a8e72fa225a0a",
+    "url": "/portfolio_3.0/static/media/Counting-Down.07849fb6.png"
+  },
+  {
+    "revision": "8c4e2746af78276ba9432419819d469d",
+    "url": "/portfolio_3.0/static/media/Lyric-Searching.8c4e2746.png"
+  },
+  {
+    "revision": "e931b4422ecfd5fda7f6c944ca8ebd27",
+    "url": "/portfolio_3.0/static/media/Meal-Finder.e931b442.png"
+  },
+  {
+    "revision": "f201488ebecf71256e693a293054cc4a",
+    "url": "/portfolio_3.0/static/media/Music-Player.f201488e.png"
+  },
+  {
+    "revision": "5ef51330f93605cd049bf3f6efd4f648",
+    "url": "/portfolio_3.0/static/media/Relaxer-App.5ef51330.png"
+  },
+  {
+    "revision": "6adf8e6d06e9172201e7f4ce61406a71",
+    "url": "/portfolio_3.0/static/media/Seat-Booking.6adf8e6d.png"
+  },
+  {
+    "revision": "1b6297173d21a3e1ae33254680a1e1db",
+    "url": "/portfolio_3.0/static/media/Speed-Typing.1b629717.png"
+  },
+  {
+    "revision": "072eb9777968da6f95b3bad7494aa10f",
+    "url": "/portfolio_3.0/static/media/Text-To-Speech.072eb977.png"
+  },
+  {
+    "revision": "6bebfb348c34253eb27d7a6d9c56b8f8",
+    "url": "/portfolio_3.0/static/media/calculator.6bebfb34.png"
+  },
+  {
+    "revision": "d82d73e70ab81c65d8dcf89b41a4a3d2",
+    "url": "/portfolio_3.0/static/media/caroGame.d82d73e7.png"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/portfolio_3.0/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "adc76cc10b179a4d3eb9787f8a65fd2d",
+    "url": "/portfolio_3.0/static/media/cover3.adc76cc1.jpg"
+  },
+  {
+    "revision": "b8c50b00e4a380765942f9cbd5b56030",
+    "url": "/portfolio_3.0/static/media/gitHubFinder.b8c50b00.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/portfolio_3.0/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/portfolio_3.0/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/portfolio_3.0/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/portfolio_3.0/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "4556ee59e0a5e8704adfe07331b18355",
+    "url": "/portfolio_3.0/static/media/todo2.4556ee59.png"
+  },
+  {
+    "revision": "7f16a3e685cbd51eb536d5ef970dfb69",
+    "url": "/portfolio_3.0/static/media/weatherApp.7f16a3e6.png"
+  }
+]);
